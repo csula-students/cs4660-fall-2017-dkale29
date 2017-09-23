3b63dbe2e8a537ad9e973b7fca373255e53a5227
Daniel Kale <dkalechip@gmail.com>
2017-09-23 16:24:02 -0700

    made fixes to tutorial for pull request

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -1,5 +1,5 @@
 """Files tests simple file read related operations"""
-
+from __future__ import division
 class SimpleFile(object):
     """SimpleFile tests using file read api to do some simple math"""
     def __init__(self, file_path):
@@ -9,30 +9,40 @@ class SimpleFile(object):
         dimension array (numbers)
         """
 
+        f = open(file_path)
+        for line in f:
+            self.numbers.append(list(map(int,line.split(" "))))
+
     def get_mean(self, line_number):
         """
         get_mean retrieves the mean value of the list by line_number (starts
         with zero)
         """
-        pass
+        return sum(self.numbers[line_number])/len(self.numbers[line_number])
 
     def get_max(self, line_number):
         """
         get_max retrieves the maximum value of the list by line_number (starts
         with zero)
         """
-        pass
+        for number in self.numbers[line_number]:
+            return(max(self.numbers[line_number]))
 
     def get_min(self, line_number):
         """
         get_min retrieves the minimum value of the list by line_number (starts
         with zero)
         """
-        pass
+        minnum = 0
+        for number in self.numbers[line_number]:
+            return(min(self.numbers[line_number]))
 
     def get_sum(self, line_number):
         """
         get_sum retrieves the sumation of the list by line_number (starts with
         zero)
         """
-        pass
+        sumofnum = 0
+        for number in self.numbers[line_number]:
+            sumofnum += number
+        return sumofnum
